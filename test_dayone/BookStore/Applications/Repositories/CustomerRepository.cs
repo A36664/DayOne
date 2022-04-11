b2a8658ca76df00b63b64cf50794b9eaa33f28c6
thaiduongngg01 <thaiduongngg01@gmail.com>
2022-04-11 22:09:16 +0700

    add unitofwork

---
+++ test_dayone/BookStore/Applications/Repositories/CustomerRepository.cs
@@ -0,0 +1,31 @@
+﻿using BookStore.Data.EF;
+using BookStore.Data.Entities;
+
+namespace BookStore.Applications.Repositories
+{
+    public class CustomerRepository
+    {
+        private BookStoreContext _context;
+        public CustomerRepository(BookStoreContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(Customer customer)
+        {
+            _context.Customers.Add(customer);
+        }
+
+        public void Remove(Customer customer)
+        {
+            _context.Customers.Remove(customer);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+
+    }
+}
---
+++ test_dayone/BookStore/Controllers/ContactsController.cs
@@ -0,0 +1,31 @@
+﻿using BookStore.Applications;
+using BookStore.Data.EF;
+using BookStore.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Controllers
+{
+    public class ContactsController : Controller
+    {
+        private readonly BookStoreContext _context;
+        public ContactsController(BookStoreContext context)
+        {
+            _context = context;
+            // dùng chung mọt instance context 
+            UnitOfWork unitOfWork = new UnitOfWork(_context);
+
+            unitOfWork.orderRepository.Add(new Order() { });
+
+            unitOfWork.CustomerRepository.Add(new Customer() { });
+
+            unitOfWork.Save();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
